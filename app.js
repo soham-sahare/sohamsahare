26961e6be2f7784a008b81fa507fd1be4b20fc30
sohamsahare <sohamsahare123@gmail.com>
2023-02-18 16:02:29 +0530

    static path changed

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const formidable = require('express-formidable');
 const expressEjsLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000;
 const dotenv = require("dotenv")
+const path = require('path')
 
 dotenv.config()
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 app.use(formidable());
 
-app.set('views', './views');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+// app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(expressEjsLayout);
 
