e827af522e801908bcb18a6aa0a98731a4addcca
sohamsahare <sohamsahare123@gmail.com>
2023-02-23 15:57:58 +0530

    projects updated

--- routes/projects.js
+++ routes/projects.js
@@ -1,28 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const data = require("../data.json");
+
 
 router.get('/', (req, res) => {
     res.render('./projects/projects');
 })
 
 router.get('/ia_management_system', (req, res) => {
-    res.render('./projects/ia_management_system');
+    res.render('./projects/project', new_data = data[0]);
 })
 
 router.get('/v_repository', (req, res) => {
-    res.render('./projects/v_repository');
+    res.render('./projects/project', new_data = data[1]);
 })
 
 router.get('/up_learn', (req, res) => {
-    res.render('./projects/up_learn');
+    res.render('./projects/project', new_data = data[2]);
 })
 
 router.get('/voom_meet', (req, res) => {
-    res.render('./projects/voom_meet');
+    res.render('./projects/project', new_data = data[3]);
 })
 
 router.get('/kooke_s_cafe', (req, res) => {
-    res.render('./projects/kooke_s_cafe');
+    res.render('./projects/project', new_data = data[4]);
+})
+
+router.get('/c_url', (req, res) => {
+    res.render('./projects/project', new_data = data[5]);
 })
 
 module.exports = router; 
