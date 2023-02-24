db524b87c301141774bf83fa5d535400402dd734
sohamsahare <sohamsahare123@gmail.com>
2023-02-24 16:11:01 +0530

    Update scripts

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.get('/', (req, res) => {
     res.render('./blogs/blogs');
 })
 
 router.get('/single', (req, res) => {
-    res.render('./blogs/single-blog');
+    res.render('./blogs/single');
 })
-
 
 module.exports = router; 
--- routes/projects.js
+++ routes/projects.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const data = require("../data.json");
 
-
 router.get('/', (req, res) => {
     res.render('./projects/projects');
 })
@@ -25,6 +24,10 @@ router.get('/voom_meet', (req, res) => {
 
 router.get('/kooke_s_cafe', (req, res) => {
     res.render('./projects/project', new_data = data[4]);
+})
+
+router.get('/c_url', (req, res) => {
+    res.render('./projects/project', new_data = data[5]);
 })
 
 router.get('/c_url', (req, res) => {
